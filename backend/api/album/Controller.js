2dff17e8718a2a17e1601b1a98df6d4bed39e6d5
Meme <meme_es@hotmail.com>
2022-07-07 17:23:05 -0600

    Setup album entities to work with MongoDB

--- backend/api/album/Controller.js
+++ backend/api/album/Controller.js
@@ -4,30 +4,30 @@ class AlbumController {
     this._entity = albumEntity
   }
 
-  getAllAlbums () {
-    const response = this._service.all('album')
+  async getAllAlbums () {
+    const response = await this._service.all('album')
     return response
   }
 
-  getAlbum (id) {
-    const response = this._service.getItem('album', id)
+  async getAlbum (id) {
+    const response = await this._service.getItem('album', id)
     return response
   }
 
-  createNewAlbum (album) {
+  async createNewAlbum (album) {
     const newAlbum = new this._entity(album)
-    const response = this._service.save('album', newAlbum)
+    const response = await this._service.save('album', newAlbum)
     return response
   }
 
-  deleteAlbum (id) {
-    const response = this._service.delete('album', id)
+  async deleteAlbum (id) {
+    const response = await this._service.delete('album', id)
     return response
   }
 
-  updateAlbum (album, id) {
+  async updateAlbum (album, id) {
     const newAlbum = new this._entity(album)
-    const response = this._service.update('album', newAlbum, id)
+    const response = await this._service.update('album', newAlbum, id)
     return response
   }
 }
--- backend/api/album/Router.js
+++ backend/api/album/Router.js
@@ -15,12 +15,12 @@ class AlbumRouter {
     this._router.get('/:id', this.handleGetAlbum.bind(this))
     this._router.post('/', albumValidations, this.handlePostAlbum.bind(this))
     this._router.delete('/:id', this.handleDeleteAlbum.bind(this))
-    this._router.put('/:id', albumValidations, this.handleUpdateAlbum.bind(this))
+    this._router.put('/:id', this.handleUpdateAlbum.bind(this))
   }
 
-  handleGetAlbums (req, res) {
+  async handleGetAlbums (req, res) {
     try {
-      const result = this._controller.getAllAlbums()
+      const result = await this._controller.getAllAlbums()
       if (result.length === 0) {
         this._response.success(req, res, 'No hay albums aún', this._httpCode.NOT_FOUND)
       } else {
@@ -31,10 +31,9 @@ class AlbumRouter {
     }
   }
 
-  handleGetAlbum (req, res) {
+  async handleGetAlbum (req, res) {
     try {
-      const albumId = parseInt(req.params.id)
-      const result = this._controller.getAlbum(albumId)
+      const result = await this._controller.getAlbum(req.params.id)
       if (result) {
         this._response.success(req, res, result, this._httpCode.OK)
       } else {
@@ -45,25 +44,24 @@ class AlbumRouter {
     }
   }
 
-  handlePostAlbum (req, res) {
+  async handlePostAlbum (req, res) {
     const errors = validationResult(req)
 
     if (errors.isEmpty()) {
       const data = req.body
-      const result = this._controller.createNewAlbum(data)
+      const result = await this._controller.createNewAlbum(data)
       this._response.success(req, res, result, this._httpCode.CREATED)
     } else {
       this._response.error(req, res, errors, this._httpCode.BAD_REQUEST)
     }
   }
 
-  handleDeleteAlbum (req, res) {
+  async handleDeleteAlbum (req, res) {
     try {
-      const albumId = parseInt(req.params.id)
-      const result = this._controller.getAlbum(albumId)
+      const result = await this._controller.deleteAlbum(req.params.id)
+
       if (result) {
-        const result = this._controller.deleteAlbum(albumId)
-        this._response.success(req, res, result, this._httpCode.OK)
+        this._response.success(req, res, 'Item deleted at albums table', this._httpCode.OK)
       } else {
         this._response.success(req, res, 'Elemento no encontrado', this._httpCode.NOT_FOUND)
       }
@@ -72,17 +70,16 @@ class AlbumRouter {
     }
   }
 
-  handleUpdateAlbum (req, res) {
+  async handleUpdateAlbum (req, res) {
     const errors = validationResult(req)
 
     if (errors.isEmpty()) {
       try {
-        const albumId = parseInt(req.params.id)
-        const result = this._controller.getAlbum(albumId)
+        const data = req.body
+        const result = await this._controller.updateAlbum(data, req.params.id)
+
         if (result) {
-          const data = req.body
-          const result = this._controller.updateAlbum(data, albumId)
-          this._response.success(req, res, result, this._httpCode.OK)
+          this._response.success(req, res, 'Item modified at albums table', this._httpCode.OK)
         } else {
           this._response.success(req, res, 'Elemento no encontrado', this._httpCode.NOT_FOUND)
         }
